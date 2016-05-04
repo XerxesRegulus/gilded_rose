a6e285ca6a9043252fef583dede8f8077080b666
John Paul Ricarse <johnpauliloilo@gmail.com>
2016-05-04 16:34:56 +0800

    add spec and basic code for brie, conjured, and default

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -21,10 +21,11 @@ class GildedRose
     end
   end
 
-
   # Seperate methods for each update style
   def update_aged_brie(item)
-
+    item.quality = item.quality + 1 if item.quality < 50 && item.sell_in > 0
+    item.quality = item.quality + 2 if item.quality > 0 && item.sell_in == 0
+    item.sell_in = item.sell_in - 1 if item.sell_in > 0
   end
 
   def update_sulfuras(item)
@@ -36,10 +37,15 @@ class GildedRose
   end
 
   def update_conjured_item(item)
-
+    item.quality = item.quality - 2 if item.quality > 0 && item.sell_in > 0
+    item.quality = item.quality - 4 if item.quality > 0 && item.sell_in == 0
+    item.sell_in = item.sell_in - 1 if item.sell_in > 0
   end
 
   def update_default_item(item)
-
+    item.quality = item.quality - 1 if item.quality > 0 && item.sell_in > 0
+    item.quality = item.quality - 2 if item.quality > 0 && item.sell_in == 0
+    item.sell_in = item.sell_in - 1 if item.sell_in > 0
   end
+
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,33 +4,52 @@ describe GildedRose do
 
   describe "#update_quality" do
     context "Sulfuras" do
-      it "does not decrease quality" do
-        sulfuras = Item.new("Sulfuras, Hand of Ragnaros", 100, 80)
-        gilded_rose = GildedRose.new([sulfuras])
+      let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", 100, 80) }
+      let(:gilded_rose) { GildedRose.new([sulfuras]) }
+
+      before do
         gilded_rose.update_quality()
+      end
+
+      it "does not decrease quality" do
         expect(sulfuras.quality).to eq(80)
       end
 
       it "does not change its sell_in value" do
-        sulfuras = Item.new("Sulfuras, Hand of Ragnaros", 100, 80)
-        gilded_rose = GildedRose.new([sulfuras])
-        expect{gilded_rose}.to_not change{sulfuras.sell_in}
+        expect(sulfuras.sell_in).to not_change(sulfuras.sell_in)
       end
-
     end
 
     context "Aged Brie" do
+      let(:aged_brie) { Item.new("Aged Brie", 10, 20) }
+      let(:old_aged_brie) { Item.new("Aged Brie", 0, 20) }
+      let(:quality_aged_brie) { Item.new("Aged Brie", 0, 50) }
+      let(:gilded_rose) { GildedRose.new([aged_brie, old_aged_brie]) }
+
+      before do
+        gilded_rose.update_quality()
+      end
+
       it "increases its quality" do
+        expect(aged_brie.quality).to eq(21)
+      end
+
+      it "increases in quality twice if sell in has passed" do
+        expect(old_aged_brie.quality).to eq(22)
       end
 
       it "reduces the sell_in value" do
+        expect(aged_brie.sell_in).to eq(9)
       end
 
       it "reduces the sell_in but not less than zero" do
+        expect(old_aged_brie.sell_in).to eq(0)
       end
 
       it "does not exceed a quality of 50" do
+        expect(quality_aged_brie.quality).to eq(50)
       end
+
     end
 
     context "Backstage Passes" do
@@ -72,6 +91,67 @@ describe GildedRose do
         gilded_rose.update_quality()
         expect(backstage_passes.quality).to eq(50)
       end
+    end
+
+    context "Conjured Items" do
+      let(:conjured) { Item.new("Conjured", 10, 20) }
+      let(:old_conjured) { Item.new("Conjured", 0, 20) }
+      let(:quality_conjured) { Item.new("Conjured", 10, 50) }
+      let(:low_quality_conjured) { Item.new("Conjured", 10, 0) }
+      let(:gilded_rose) { GildedRose.new([conjured, old_conjured, quality_conjured, low_quality_conjured]) }
+
+      before do
+        gilded_rose.update_quality()
+      end
+
+      it "degrades in quality as fast as normal items" do
+        expect(conjured.quality).to eq(18)
+      end
+
+      it "reduces the sell_in value" do
+        expect(conjured.sell_in).to eq(9)
+      end
+
+      it "reduces the sell_in but not less than zero" do
+        expect(old_conjured.sell_in).to eq(0)
+      end
+
+      it "should degrade twice as fast if sell_in has passed" do
+        expect(old_conjured.quality).to eq(16)
+      end
+    end
+
+    context "Default Items" do
+      let(:default_item) { Item.new("sample", 10, 20) }
+      let(:old_default_item) { Item.new("sample", 0, 20) }
+      let(:quality_default_item) { Item.new("sample", 10, 50) }
+      let(:low_quality_default_item) { Item.new("sample", 10, 0) }
+      let(:gilded_rose) { GildedRose.new([default_item, old_default_item, quality_default_item, low_quality_default_item]) }
+
+      before do
+        gilded_rose.update_quality()
+      end
+
+      it "degrades in quality" do
+        expect(default_item.quality).to eq(19)
+      end
+
+      it "reduces in sell_in value" do
+        expect(default_item.sell_in).to eq(9)
+      end
+
+      it "does not have quality less than 0" do
+        expect(low_quality_default_item.quality).to eq(0)
+      end
+
+      it "reduces the sell_in but not less than zero" do
+        expect(old_default_item.sell_in).to eq(0)
+      end
+
+      it "should degrade twice as fast if sell_in has passed" do
+        expect(old_default_item.quality).to eq(18)
+      end
+
     end
   end
 
