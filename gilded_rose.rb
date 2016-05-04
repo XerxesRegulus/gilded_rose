5e19b804fe71f5a33eb1e33eb139251b04295d7f
John Paul Ricarse <johnpauliloilo@gmail.com>
2016-05-04 14:48:58 +0800

    add circle yml (#5)

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,64 +6,21 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      case item.name
+      when "Aged Brie"
+        update_aged_brie(item)
+      when "Sulfuras, Hand of Ragnaros"
+        update_sulfuras(item)
+      when "Backstage passes to a TAFKAL80ETC concert"
+        update_backstage_passes(item)
+      when "Conjured"
+        update_conjured_item(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        update_default_item(item)
       end
     end
   end
 
-  case item.name
-  when "Aged Brie"
-    update_aged_brie(item)
-  when "Sulfuras, Hand of Ragnaros"
-    update_sulfuras(item)
-  when "Backstage passes to a TAFKAL80ETC concert"
-    update_backstage_passes(item)
-  when "Conjured"
-    update_conjured_item(item)
-  else
-    update_default_item(item)
-  end
 
   # Seperate methods for each update style
   def update_aged_brie(item)
